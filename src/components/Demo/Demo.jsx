15b0bb8eeab725802f4a6ba3e71fee9e2baf6cca
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-17 11:05:34 -0300

    Class 153 - A Closer Look At Child Component Re-Evaluation

---
+++ src/components/Demo/Demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DemoOutput = ({ show }) => {
+  console.log('DemoOutput Running!')
+
+  return (<p>{show ? 'This is new!' : ''}</p>);
+};
+
+export default DemoOutput;
